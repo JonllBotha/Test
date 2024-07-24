8b7bc45822ff96b5c6886724b3948aba911dde02
s22524385 <s225214385@mandela.ac.za>
2024-07-24 23:03:16 +0200

    beautifulsoup

--- Testing.py
+++ Testing.py
@@ -1,3 +1,5 @@
-print("hello world")
-print("Dance Dance Revolution")
-print()
+import requests  # type: ignore
+from bs4 import BeautifulSoup  # type: ignore
+URL = "https://olympics.com/en/paris-2024/schedule/"
+r = requests.get(URL)
+print(r.content)
