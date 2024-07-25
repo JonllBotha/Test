76adc8a6838408c36d1206f12278a33926e0c97b
s22524385 <s225214385@mandela.ac.za>
2024-07-25 13:23:00 +0200

    think it works

--- Testing.py
+++ Testing.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://olympics.com/en/paris-2024/schedule/'
-response = requests.get(url)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+response = requests.get(url, headers=headers)
+
 soup = BeautifulSoup(response.content, 'html.parser')
 
 print("Title:", soup.title.string)
