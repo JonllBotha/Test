b6628b3f25e644df93b871c2cf74991f0adf0098
s22524385 <s225214385@mandela.ac.za>
2024-07-25 13:17:47 +0200

    new version

--- Testing.py
+++ Testing.py
@@ -1,5 +1,12 @@
-import requests 
-from bs4 import BeautifulSoup  
-URL = "https://olympics.com/en/paris-2024/schedule/" ; {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'}
-r = requests.get(URL)
-print(r.content)
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://olympics.com/en/paris-2024/schedule/'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+print("Title:", soup.title.string)
+
+for link in soup.find_all('a'):
+    print("Link:", link.get('href'))
+
