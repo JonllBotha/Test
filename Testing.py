33b15ae09de3237d706707debad0d5d22cca01a5
s22524385 <s225214385@mandela.ac.za>
2024-07-25 13:09:38 +0200

    struggling

--- Testing.py
+++ Testing.py
@@ -1,5 +1,5 @@
 import requests 
 from bs4 import BeautifulSoup  
-URL = "https://olympics.com/en/paris-2024/schedule/"
+URL = "https://olympics.com/en/paris-2024/schedule/" ; {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'}
 r = requests.get(URL)
 print(r.content)
