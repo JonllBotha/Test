0e7c517551e0053a0e1e48a830dbbe4a982fd64b
s22524385 <s225214385@mandela.ac.za>
2024-07-30 13:21:41 +0200

    main code update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-if __name__ == ['__main__']:
+if __name__ == '__main__':
     url = 'https://olympics.com/en/paris-2024/schedule/'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
